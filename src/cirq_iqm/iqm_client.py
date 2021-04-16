c2eb140be010d210f5581d7cceaa4a148b2ab212
maxim <maxim-ext@meetiqm.com>
2021-04-16 16:57:30 +0300

    backend compatibility + real circuit example

--- src/cirq_iqm/iqm_client.py
+++ src/cirq_iqm/iqm_client.py
@@ -9,49 +9,38 @@ BASE_URL = "https://meetiqm.com/api/"
 class IQMException(Exception):
     pass
 
-class APISubmissionError(IQMException):
-    pass
-
 class ApiTimeoutError(IQMException):
     pass
 
-class ApiServerError(IQMException):
-    pass
-
-
 class RunStatus(str, Enum):
-    RUNNING="running"
-    SUCCESS="success"
-    FAILURE="failure"
+    PENDING="pending"
+    READY="ready"
+    FAILED="failed"
 
 class IQMBackendClient:
     def __init__(self, token):
         self._token=token
 
-    def submit_circuit(self,circuit_definition, mapping, operations, repetitions:int=1) -> int:
+    def submit_circuit(self, mapping:dict, circuit:dict, shots:int=1) -> int:
         result = requests.post(f"{BASE_URL}/circuit/run", data={
-            "circuit_definition": circuit_definition,
             "mapping": mapping,
-            "operations": "",
-            "repetitions": repetitions
+            "circuit": circuit,
+            "shots": shots
         })
-        if result.status_code is not 201:
-            raise RunSubmissionError(f"{result.status_code}: {result.reason}")
+        result.raise_for_status()
 
-        return json.loads(result.text)["run_id"]
+        return json.loads(result.text)["id"]
 
     def get_run_status(self, id) -> dict:
         result = requests.get(f"{BASE_URL}/circuit/run/{id}")
-        if result.status_code is not 200:
-            raise ApiServerError(result.reason)
-
+        result.raise_for_status()
         return json.loads(result.text)
 
     def wait_results(self,id, timeout_secs=10)->dict:
         start_time=datetime.now()
         while (datetime.now()-start_time).total_seconds()<timeout_secs:
             results=self.get_run_status(id)
-            if results["status"] != RunStatus.RUNNING:
+            if results["status"] != RunStatus.PENDING:
                 return results
         raise ApiTimeoutError(f"The task didn't finish in {timeout_secs} seconds")
 
--- tests/iqm_client_test.py
+++ tests/iqm_client_test.py
@@ -12,52 +12,94 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Testing IQM client.
-"""
+"""Testing IQM api client."""
 import json
 
 import pytest
-from cirq_iqm.iqm_client import IQMBackendClient, ApiServerError
+from cirq_iqm.iqm_client import IQMBackendClient
 from mockito import when, mock, unstub
 import requests
+from requests import HTTPError
 
 BASE_URL = "https://meetiqm.com/api/"
 api_key = "random_key"
 
+
 @pytest.fixture(scope="function", autouse=True)
 def prepare():
-    success_submit_response = mock({'status_code': 201, 'text': json.dumps({"run_id": 14})})
+    success_submit_response = mock({'status_code': 201, 'text': json.dumps({"id": 14})})
     when(requests).post(f"{BASE_URL}/circuit/run", ...).thenReturn(success_submit_response)
 
-    running_response = mock({'status_code': 200, 'text': json.dumps({"status":"running"})})
-    success_get_response = mock({'status_code': 200, 'text': json.dumps({"status":"success","results":"0101010101"})})
+    running_response = mock({'status_code': 200, 'text': json.dumps({"status": "pending"})})
+    success_get_response = mock({'status_code': 200, 'text': json.dumps({"status": "ready", "results": "0101010101"})})
     no_run_response = mock({'status_code': 404, 'text': "Run not found"})
 
     when(requests).get(f"{BASE_URL}/circuit/run/14", ...).thenReturn(running_response).thenReturn(success_get_response)
     when(requests).get(f"{BASE_URL}/circuit/run/13", ...).thenReturn(no_run_response)
 
-    yield # running test function
+    yield  # running test function
 
     unstub()
 
+
+mapping = dict()
+
+
 def test_submit_circuit():
     client = IQMBackendClient(api_key)
-    run_id = client.submit_circuit("circuit_definition", "mapping", "operations", repetitions=1000)
+    run_id = client.submit_circuit({"Qubit A": "qubit_1", "Qubit B": "qubit_2"}, {
+        "name": "The circuit",
+        "args": {
+            "alpha": 1.2
+        },
+        "instructions": [
+            {
+                "name": "cz",
+                "qubits": [
+                    "Qubit A",
+                    "Qubit B"
+                ],
+                "args": {}
+            },
+            {
+                "name": "rotation",
+                "qubits": [
+                    "Qubit A"
+                ],
+                "args": {
+                    "phase_t": 1.22,
+                    "angle_t": {
+                        "expr": "{{alpha}}/2"
+                    }
+                }
+            },
+            {
+                "name": "measurement",
+                "qubits": [
+                    "Qubit A"
+                ],
+                "args": {
+                    "output_label": "A"
+                }
+            }
+        ]
+    }, shots=1000)
     assert run_id == 14
 
 
 def test_get_run_status():
     client = IQMBackendClient(api_key)
-    assert client.get_run_status(14)["status"]=="running"
-    assert client.get_run_status(14)["status"] == "success"
+    assert client.get_run_status(14)["status"] == "pending"
+    assert client.get_run_status(14)["status"] == "ready"
 
 
 def test_wrong_run():
+    unstub()
     client = IQMBackendClient(api_key)
-    with pytest.raises(ApiServerError):
+    with pytest.raises(HTTPError):
         assert client.get_run_status(13)
 
 
 def test_wait_results():
     client = IQMBackendClient(api_key)
-    assert client.wait_results(14)["status"] == "success"
+    assert client.wait_results(14)["status"] == "ready"
