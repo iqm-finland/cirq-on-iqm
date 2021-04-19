fc5a06cfa15220a0937a935369c89ea63b63a9ee
maxim <maxim-ext@meetiqm.com>
2021-04-19 15:49:16 +0300

    demo uses mocking and a simpler circuit

--- examples/demo_iqm_engine.py
+++ examples/demo_iqm_engine.py
@@ -22,29 +22,19 @@ Demonstrates calling an IQM device.
 from cirq_iqm.adonis import Adonis
 import cirq
 from cirq_iqm.iqm_device import IQMQubit
-import cirq_iqm.hardware
-from cirq_iqm.valkmusa import Valkmusa
-from cirq_iqm.extended_qasm_parser import circuit_from_qasm
 from cirq_iqm import hardware
+
+# todo: remove mocking when the server is deployed
+import tests.coco_mock
+tests.coco_mock.mock_backend()
+
 
 qubit = IQMQubit(1)
 circuit = cirq.Circuit(
-    cirq.X(qubit)**0.5,                 # Square root of NOT.
-    cirq.measure(qubit, key='result')   # Measurement.
+    cirq.X(qubit) ** 0.5,  # Square root of NOT.
+    cirq.measure(qubit, key='result')  # Measurement.
 )
 
-
-qasm_program = """
-        OPENQASM 2.0;
-        include "qelib1.inc";
-        qreg q[3];
-        creg meas[3];
-        U(0.2, 0.5, 1.7) q[1];
-        h q[0];
-        cx q[2], q[1];
-        ising(-0.6) q[0], q[2];  // QASM extension
-    """
-circuit = circuit_from_qasm(qasm_program)
 device = Adonis()
 circuit_adonis = device.map_circuit(circuit)
 
