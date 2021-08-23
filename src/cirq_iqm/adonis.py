b02c36532a8e9c8eab17f7f9a1cb76d332ac3362
Ville Bergholm <1044345+smite@users.noreply.github.com>
2021-08-23 21:30:23 +0300

    Rz is not native for Adonis.

--- src/cirq_iqm/adonis.py
+++ src/cirq_iqm/adonis.py
@@ -63,13 +63,28 @@ class Adonis(idev.IQMDevice):
         ops.PhasedXPowGate,
         ops.XPowGate,
         ops.YPowGate,
-        ops.ZPowGate,
         ops.MeasurementGate
     )
 
     NATIVE_GATE_INSTANCES = (
         ops.CZPowGate(),
     )
+
+    # We postpone the decomposition of the z rotations until the final stage of optimization,
+    # since usually they are eliminated by the EjectZ/DropRZBeforeMeasurement optimization stages.
+    DECOMPOSE_FINALLY = (ops.ZPowGate,)
+
+    def operation_final_decomposer(self, op: ops.Operation) -> Optional[list[cirq.Operation]]:
+        # Decomposes z rotations using x and y rotations.
+        if isinstance(op.gate, ops.ZPowGate):
+            # Rz using Rx, Ry
+            q = op.qubits[0]
+            return [
+                ops.XPowGate(exponent=-0.5).on(q),
+                ops.YPowGate(exponent=op.gate.exponent).on(q),
+                ops.XPowGate(exponent=0.5).on(q),
+            ]
+        raise NotImplementedError(f'Decomposition missing: {op.gate}')
 
     def operation_decomposer(self, op: cirq.Operation) -> Optional[list[cirq.Operation]]:
         # Decomposes gates into the native Adonis gate set.
