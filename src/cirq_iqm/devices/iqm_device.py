2dd90827ef3977d6ed46458173795fb97e6d4c76
Ville Bergholm <1044345+smite@users.noreply.github.com>
2022-11-02 19:16:12 +0200

    Cleanup.

--- src/cirq_iqm/devices/iqm_device.py
+++ src/cirq_iqm/devices/iqm_device.py
@@ -22,7 +22,7 @@ from __future__ import annotations
 
 import collections.abc as ca
 from math import pi as PI
-from typing import FrozenSet, Optional, Union
+from typing import Optional, Union
 import uuid
 
 import cirq
@@ -261,10 +261,6 @@ class IQMDevice(devices.Device):
                 raise ValueError(f'Qubit not on device: {qubit!r}')
 
         self.check_qubit_connectivity(operation)
-
-    @staticmethod
-    def _qubit_set_from_count(qubit_count: int) -> FrozenSet[cirq.NamedQubit]:
-        return frozenset(cirq.NamedQubit.range(1, qubit_count + 1, prefix=IQMDeviceMetadata.QUBIT_NAME_PREFIX))
 
     def __eq__(self, other):
         return self.__class__ == other.__class__ and self._metadata == other._metadata
