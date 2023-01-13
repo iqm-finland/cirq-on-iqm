61b68e31c84eff346ede14f7d9999b2812b7446e
Ville Bergholm <1044345+smite@users.noreply.github.com>
2023-01-13 19:01:52 +0200

    Update the execution demo.

--- examples/demo_iqm_execution.py
+++ examples/demo_iqm_execution.py
@@ -11,53 +11,57 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """
 Demonstrates executing a quantum circuit on an IQM quantum computer.
 
 Set the IQM_SERVER_URL environment variable before running this script.
-Also, if the server you are running against requires authentication, you will also have to set IQM_AUTH_SERVER,
-IQM_AUTH_USERNAME and IQM_AUTH_PASSWORD.
+Also, if the server you are running against requires authentication you will also have to set
+IQM_AUTH_SERVER, and either IQM_TOKENS_FILE or both of IQM_AUTH_USERNAME and IQM_AUTH_PASSWORD.
+
 E.g.
-    export IQM_SERVER_URL="https://example.com"
-    export IQM_AUTH_SERVER="https://auth.example.com"; export IQM_AUTH_USERNAME="my username";
-    export IQM_AUTH_PASSWORD="my password"
+
+    export IQM_SERVER_URL="https://example.com/cocos"
+    export IQM_AUTH_SERVER="https://example.com/auth"
+    export IQM_TOKENS_FILE="/path/to/my/tokens.json"
 """
 import os
 
 import cirq
 
-from cirq_iqm.devices import Adonis
 from cirq_iqm.iqm_sampler import IQMSampler
+from cirq_iqm.optimizers import simplify_circuit
 
 
-def demo_adonis_run():
+def demo_run_circuit():
     """
-    Run a quantum circuit on an Adonis quantum computer.
+    Run a quantum circuit on an IQM quantum computer.
     """
     a = cirq.NamedQubit('Alice')
     b = cirq.NamedQubit('Bob')
+    circuit = cirq.Circuit(cirq.X(a) ** 0.5, cirq.CX(a, b), cirq.measure(a, b, key='m'))
+    print('Original circuit:\n')
+    print(circuit)
 
-    circuit = cirq.Circuit(cirq.X(a) ** 0.5, cirq.Z(a), cirq.measure(a, b, key='result'))
+    sampler = IQMSampler(os.environ['IQM_SERVER_URL'])
 
-    device = Adonis()
-    circuit_decomposed = device.decompose_circuit(circuit)
-    print(circuit_decomposed)
-
-    sampler = IQMSampler(os.environ['IQM_SERVER_URL'], device)
+    circuit_decomposed = sampler.device.decompose_circuit(circuit)
+    circuit_routed = sampler.device.route_circuit(circuit_decomposed)
+    circuit = simplify_circuit(circuit_routed)
+    print('\nTranspiled and routed circuit:\n')
+    print(circuit)
+    print('\n')
 
     # This will send the circuit to the server to be run, and return a cirq.study.Result
     # containing the measurement results.
-    results = sampler.run(circuit_decomposed, repetitions=1000)
+    results = sampler.run(circuit, repetitions=1000)
 
     # Sampler results can be accessed several ways
-
     # For instance, to see the histogram of results
-    print(results.histogram(key='result'))
+    print(results.histogram(key='m', fold_func=lambda x: tuple(x)))
 
     # Or the data itself
     print(results.data)
 
 
 if __name__ == '__main__':
-    demo_adonis_run()
+    demo_run_circuit()
